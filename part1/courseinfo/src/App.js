45f43cbf294a43a9f151c1689b58fca313e0f5a9
Frank Tunde <frankie@Frankie.local>
2022-01-04 22:43:56 +0200

    1.3: course information, step 3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,29 +9,35 @@ const Total = (props) =>{
 }
 
 const Part = (props) =>{
-  return <p>{props.part} {props.exercise}</p>;
+  return <p>{props.name} {props.exercise}</p>;
 }
 
 const Content = (props) =>{
   return(
     <div>
-      <Part part = {props.part1} exercise = {props.exercises1}/>
-      <Part part = {props.part2} exercise = {props.exercises2}/>
-      <Part part = {props.part3} exercise = {props.exercises3}/>
+      <Part name = {props.part1.name} exercise = {props.part1.exercises}/>
+      <Part name = {props.part2.name} exercise = {props.part2.exercises}/>
+      <Part name = {props.part3.name} exercise = {props.part3.exercises}/>
     </div> 
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const total = exercises1 + exercises2 + exercises3
+  const total = part1.exercises + part2.exercises + part3.exercises
 
   return (
     <div>
@@ -40,9 +46,6 @@ const App = () => {
         part1 = {part1}
         part2 = {part2}
         part3 = {part3}
-        exercises1 = {exercises1}
-        exercises2 = {exercises2}
-        exercises3 = {exercises3}
       />
       <Total total={total}/>
     </div>
