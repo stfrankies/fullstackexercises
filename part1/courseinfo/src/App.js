5e090e3c04b385dd9b002059426dbe792aee1723
Frank Tunde <frankie@Frankie.local>
2022-01-04 23:07:02 +0200

    1.5: course information, step 5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,8 +4,11 @@ const Header = (props) =>{
   return <h1>{props.course}</h1>;
 }
 
-const Total = (props) =>{
-  return<p>Number of exercises {props.total}</p>;
+const Total = ({parts}) =>{
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return(
+    <p>Number of exercises {total}</p>
+  );
 }
 
 const Part = (props) =>{
@@ -23,32 +26,29 @@ const Content = (props) =>{
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content 
-        parts = {parts}
-      />
-      <Total total={total}/>
+      <Header course = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
