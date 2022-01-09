4662109777a0a5dedf8ceb82e7af30fdbfd947fd
Frank Tunde <frankie@Frankie.local>
2022-01-09 23:35:48 +0200

    2.4: course information, step 9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -20,7 +20,7 @@ const Part = (props) =>{
 const Content = (props) =>{
   return(
     <div>
-      {props.parts.map((part, i) =><Part key={i} name={part.name} exercise ={part.exercises}/>)}
+      {props.parts.map(part =><Part key={part.id} name={part.name} exercise ={part.exercises}/>)}
     </div> 
   )
 }
@@ -36,33 +36,59 @@ const Course = ({course}) =>{
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
 
   // const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
 
   return (
-    <Course course={course}/>
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course}/>)}
+    </div>
   )
 }
 
