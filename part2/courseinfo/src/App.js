667ac2c0ce3d56c2834babcc79853eea0ab290a0
Frank Tunde <frankie@Frankie.local>
2022-01-09 23:17:38 +0200

    2.2-2.3: course information, step 7-8

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -4,6 +4,14 @@ const Header = (props) =>{
   return <h1>{props.name}</h1>;
 }
 
+const Total = ({parts}) =>{
+  const total = parts.map((part)=>{return part.exercises}).reduce((part, next)=>{
+    return part + next;
+  })
+  return(
+    <h4>total of {total} exercises</h4>
+  )
+}
 
 const Part = (props) =>{
   return <p>{props.name} {props.exercise}</p>;
@@ -22,6 +30,7 @@ const Course = ({course}) =>{
     <div>
       <Header name={course.name}/>
       <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
