cdf383ee84edb02840719f3a229c95691efc43f1
Frank Tunde <frankie@Frankie.local>
2022-06-20 14:28:42 +0300

    Ex.4.8: Blog list test, step 1

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -12,21 +12,23 @@ app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
+      response.status(200).json(blogs)
     })
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
 
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = app
