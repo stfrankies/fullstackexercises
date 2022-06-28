9487feda6cce051cc83ed633848c1a07160e549b
Frank Tunde <frankie@Frankie.local>
2022-06-28 23:39:12 +0300

    Ex.4.10: Blog lists tests, step 3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,14 +1,44 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Blog = require('../model/blogs')
 const app = require('../index')
 
 const api = supertest(app)
+
+const initialBlogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          author: "Michael Chan",
+          url: "https://reactpatterns.com/",
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+          likes: 5,
+          __v: 0
+        },  
+      ]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(response.body.length)
 })
 
 test("blog has a unique id named 'id'", async () => {
@@ -16,6 +46,22 @@ test("blog has a unique id named 'id'", async () => {
   expect(response.body[0].id).toBeDefined();
 
 });
+
+test('valid blog can be added', async () => {
+  const newBlog = {
+          title: "Type warship",
+          author: "Roberto C. Martino",
+          url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWarship.html",
+          likes: 5,
+        }
+  await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length + 1) 
+})
 
 afterAll(() => {
   mongoose.connection.close()
