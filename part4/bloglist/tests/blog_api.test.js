6b81b26ef229e9efbfcec647fa4950af73ce4b03
stfrankies <63299700+stfrankies@users.noreply.github.com>
2022-08-16 19:14:54 +0300

    Ex.4.17: Bloglist expansion, step 4 - user/blog id added in test

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -6,23 +6,25 @@ const app = require('../app')
 const api = supertest(app)
 
 const initialBlogs = [
-        {
-          _id: "5a422a851b54a676234d17f7",
-          title: "React patterns",
-          author: "Michael Chan",
-          url: "https://reactpatterns.com/",
-          likes: 7,
-          __v: 0
-        },
-        {
-          _id: "5a422aa71b54a676234d17f8",
-          title: "Go To Statement Considered Harmful",
-          author: "Edsger W. Dijkstra",
-          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-          likes: 5,
-          __v: 0
-        },  
-      ]
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    user: "62d2f6ec99e8f00977a4b72e",
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    user: "62d2f6ec99e8f00977a4b730",
+    __v: 0
+  },
+]
       
 beforeEach(async () => {
   await Blog.deleteMany({})
