36ad5206ebe3d29bd8001f91699c268859af34a3
Frank Tunde <frankie@Frankie.local>
2022-08-16 14:20:06 +0300

    Ex.4.15: Bloglist Expansion, step 3, refactor and corrected... working

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,32 +1,8 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controller/blog')
-const usersRouter = require('./controller/user')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./app')
 
-logger.info('connecting to', config.MONGODB_URI)
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-module.exports = app
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
