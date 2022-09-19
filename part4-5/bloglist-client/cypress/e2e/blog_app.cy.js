d0d9057d7839db7530260e2be60a86d9de96abe6
Frank Tunde <frankie@Frankie.local>
2022-09-19 14:58:14 +0300

    Ex.5.18: Bloglist end to end tests, step 2

--- part4-5/bloglist-client/cypress/e2e/blog_app.cy.js
+++ part4-5/bloglist-client/cypress/e2e/blog_app.cy.js
@@ -2,12 +2,20 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    const user = {
+    const user1 = {
       name: 'Matti Luukkainen',
       username: 'mluukkai',
       password: 'salainen'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.request('POST', 'http://localhost:3003/api/users/', user1)
+
+    const user2 = {
+        name:'Jackie Chan',
+        username:'jackie_chan',
+        password:'chanchan'
+    }
+
+     cy.request('POST', 'http://localhost:3003/api/users/', user2)
     
     cy.visit('http://localhost:3000')
   })
@@ -17,5 +25,25 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+        cy.get('input:first').type('jackie_chan')
+        cy.get('input:last').type('chanchan')
+        cy.contains('login').click()
+
+        cy.contains('Jackie Chan logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('jackie_chan')
+        cy.get('input:last').type('chan')
+        cy.contains('login').click()
+
+        cy.contains('invalid username or password')
+        .and('have.css', 'border-style', 'solid')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
